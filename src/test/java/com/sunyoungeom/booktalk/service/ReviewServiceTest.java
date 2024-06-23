995f5ea724edbb7a6980b3c76a497eedfa5f7549
sunyoung Eom <sunyoung.eom@outlook.com>
2024-06-23 18:46:37 +0900

    test: ReviewServiceTest 수정
    - 좋아요 기능 테스트 추가

--- src/test/java/com/sunyoungeom/booktalk/service/ReviewServiceTest.java
+++ src/test/java/com/sunyoungeom/booktalk/service/ReviewServiceTest.java
@@ -1,8 +1,10 @@
 package com.sunyoungeom.booktalk.service;
 
 import com.sunyoungeom.booktalk.domain.Review;
+import com.sunyoungeom.booktalk.domain.ReviewLikes;
 import com.sunyoungeom.booktalk.exception.ReviewException;
-import com.sunyoungeom.booktalk.repository.ReviewRepositoryMemory;
+import com.sunyoungeom.booktalk.facade.ReviewFacade;
+import com.sunyoungeom.booktalk.repository.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,17 +17,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class ReviewServiceTest {
 
     ReviewService service;
-    ReviewRepositoryMemory repository;
+    ReviewFacade reviewFacade;
+    ReviewRepositoryMemory reviewRepository = new ReviewRepositoryMemory();
+    ReviewLikesRepositoryMemory reviewLikesRepository = new ReviewLikesRepositoryMemory();
 
     @BeforeEach
     public void beforeEach() {
-        repository = new ReviewRepositoryMemory();
-        service = new ReviewService(repository);
+        reviewFacade = new ReviewFacade(reviewRepository, reviewLikesRepository);
+        service = new ReviewService(reviewFacade);
     }
 
     @AfterEach
     public void clear() {
-        repository.clearStore();
+        reviewFacade.clearStore();
     }
 
     @Test
@@ -33,7 +37,7 @@ class ReviewServiceTest {
         Review review = new Review(0L, "리뷰제목", "닉네임", "리뷰내용");
         Review createdReview = service.createReview(review, review.getUserId(), review.getAuthor());
 
-        Review result = repository.findById(createdReview.getId()).get();
+        Review result = reviewFacade.findReviewById(createdReview.getId()).get();
         assertEquals(review.getTitle(), result.getTitle());
     }
 
@@ -65,12 +69,12 @@ class ReviewServiceTest {
     void 인기순_조회() {
         Review review1 = new Review(0L, "리뷰제목", "닉네임", "리뷰내용");
         Review review2 = new Review(1L, "리뷰제목", "닉네임", "리뷰내용");
-        review1.setLikes(1);
-        service.createReview(review1, review1.getUserId(), review1.getAuthor());
+        Review createdReview1 = service.createReview(review1, review1.getUserId(), review1.getAuthor());
         service.createReview(review2, review2.getUserId(), review2.getAuthor());
+        service.likeReview(createdReview1.getId(), 1L);
 
         List<Review> result = service.findAllOrderByLikesDesc();
-
+        System.out.println(result.toString());
         assertThat(result.get(0).getLikes()).isGreaterThan(result.get(1).getLikes());
     }
 
@@ -122,4 +126,52 @@ class ReviewServiceTest {
         ReviewException e = assertThrows(ReviewException.class, () -> service.deleteReview(review.getId(), 1L));
         assertThat(e.getMessage()).isEqualTo("권한이 없습니다.");
     }
+
+
+    @Test
+    void 좋아요() {
+        Long userid = 1L;
+        Review review = new Review(0L, "리뷰제목", "닉네임", "리뷰내용");
+        Review createdReview = service.createReview(review, review.getUserId(), review.getAuthor());
+
+        service.likeReview(createdReview.getId(), userid);
+
+        List<ReviewLikes> resultLikes = service.findAllReviewLikes();
+        List<Review> resultReviews = service.findAllOrderByDateDesc();
+        assertThat(resultLikes.size()).isEqualTo(1);
+        assertThat(resultReviews.size()).isEqualTo(1);
+        assertThat(resultReviews.get(0).getLikes()).isEqualTo(1);
+    }
+
+    @Test
+    void 없는_리뷰에_좋아요() {
+        Long userid = 1L;
+
+        ReviewException e = assertThrows(ReviewException.class, () -> service.likeReview(0L, userid));
+        assertThat(e.getMessage()).isEqualTo("존재하지 않는 리뷰입니다.");
+    }
+
+    @Test
+    void 본인작성_리뷰에_좋아요() {
+        Long userid = 0L;
+        Review review = new Review(0L, "리뷰제목", "닉네임", "리뷰내용");
+        Review createdReview = service.createReview(review, review.getUserId(), review.getAuthor());
+
+        ReviewException e = assertThrows(ReviewException.class, () -> service.likeReview(createdReview.getId(), userid));
+        assertThat(e.getMessage()).isEqualTo("본인이 쓴 리뷰에는 좋아요를 누를 수 없습니다.");
+    }
+
+    @Test
+    void 이미_리뷰에_좋아요() {
+        Long userid = 1L;
+        Review review = new Review(0L, "리뷰제목", "닉네임", "리뷰내용");
+        Review createdReview = service.createReview(review, review.getUserId(), review.getAuthor());
+
+        service.likeReview(createdReview.getId(), userid);
+        service.likeReview(createdReview.getId(), userid);
+
+        List<ReviewLikes> result = service.findAllReviewLikes();
+        assertThat(result.size()).isEqualTo(0);
+    }
+
 }
