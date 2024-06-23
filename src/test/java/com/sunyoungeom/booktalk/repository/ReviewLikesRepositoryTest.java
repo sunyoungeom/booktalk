845a437de10b63887ae64aa827fd8e0d88c78ab8
sunyoung Eom <sunyoung.eom@outlook.com>
2024-06-23 20:29:42 +0900

    test: 좋아요 테스트 수정
    - DB 연결

--- src/test/java/com/sunyoungeom/booktalk/repository/ReviewLikesRepositoryTest.java
+++ src/test/java/com/sunyoungeom/booktalk/repository/ReviewLikesRepositoryTest.java
@@ -3,29 +3,35 @@ package com.sunyoungeom.booktalk.repository;
 import com.sunyoungeom.booktalk.domain.ReviewLikes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 class ReviewLikesRepositoryTest {
 
     @Autowired
-    ReviewLikesRepositoryMemory repository = new ReviewLikesRepositoryMemory();
+    ReviewLikesRepository repository;
 
     @Test
+    @Transactional
     void 저장() {
-        ReviewLikes reviewLikes = new ReviewLikes(1L, 1L); // 새로운 ReviewLikes 객체 생성
+        ReviewLikes reviewLikes = new ReviewLikes(1L, 1L);
 
-        ReviewLikes savedReviewLikes = repository.save(reviewLikes); // 저장
+        ReviewLikes savedReviewLikes = repository.save(reviewLikes);
 
         List<ReviewLikes> result = repository.findAll();
         assertThat(result.size()).isEqualTo(1);
     }
     @Test
+    @Transactional
     void 취소() {
-        ReviewLikes reviewLikes = new ReviewLikes(1L, 1L); // 새로운 ReviewLikes 객체 생성
-        ReviewLikes savedReviewLikes = repository.save(reviewLikes); // 저장
+        ReviewLikes reviewLikes = new ReviewLikes(1L, 1L);
+        ReviewLikes savedReviewLikes = repository.save(reviewLikes);
 
         repository.delete(savedReviewLikes.getUserId(), savedReviewLikes.getReviewId());
 
@@ -34,9 +40,10 @@ class ReviewLikesRepositoryTest {
     }
 
     @Test
+    @Transactional
     void 유저아이디로_찾기() {
-        ReviewLikes reviewLikes = new ReviewLikes(1L, 1L); // 새로운 ReviewLikes 객체 생성
-        ReviewLikes savedReviewLikes = repository.save(reviewLikes); // 저장
+        ReviewLikes reviewLikes = new ReviewLikes(1L, 1L);
+        ReviewLikes savedReviewLikes = repository.save(reviewLikes);
 
         List<ReviewLikes> result = repository.findReviewLikesByUserId(savedReviewLikes.getUserId());
 
