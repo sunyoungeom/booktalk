fbc7d7aa9f1f89ce7b5b1fca42ebead371eb8c36
sunyoung Eom <sunyoung.eom@outlook.com>
2024-06-23 18:45:14 +0900

    refactor: ReviewService에 facade-pattern 적용

---
+++ src/main/java/com/sunyoungeom/booktalk/facade/ReviewFacade.java
@@ -0,0 +1,89 @@
+package com.sunyoungeom.booktalk.facade;
+
+
+import com.sunyoungeom.booktalk.domain.Review;
+import com.sunyoungeom.booktalk.domain.ReviewLikes;
+import com.sunyoungeom.booktalk.repository.ReviewLikesRepository;
+import com.sunyoungeom.booktalk.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class ReviewFacade {
+    private final ReviewRepository reviewRepository;
+    private final ReviewLikesRepository reviewLikesRepository;
+
+    public Review saveReview(Review review) {
+        return reviewRepository.save(review);
+    }
+
+    public Optional<Review> findReviewById(Long id) {
+        return reviewRepository.findById(id);
+    }
+
+    public boolean existsReviewByTitleAndUserId(String title, Long userId) {
+        return reviewRepository.existsByTitleAndUserId(title, userId);
+    }
+
+    public List<Review> findAllReviewsOrderByDateDesc() {
+        return reviewRepository.findAllOrderByDateDesc();
+    }
+
+    public List<Review> findAllReviewsOrderByLikesDesc() {
+        return reviewRepository.findAllOrderByLikesDesc();
+    }
+
+    public List<Review> findReviewsByTitleOrderByDateDesc(String title) {
+        return reviewRepository.findByTitleOrderByDateDesc(title);
+    }
+
+    public List<Review> findReviewsByTitleOrderByLikesDesc(String title) {
+        return reviewRepository.findByTitleOrderByLikesDesc(title);
+    }
+
+    public List<Review> findReviewsByUserId(Long userId) {
+        return reviewRepository.findByUserId(userId);
+    }
+
+    public void updateReviewContent(Long id, String content) {
+        reviewRepository.update(id, content);
+    }
+
+    public void deleteReviewById(Long id) {
+        reviewRepository.delete(id);
+    }
+
+    public ReviewLikes saveReviewLikes(ReviewLikes reviewLikes) {
+        return reviewLikesRepository.save(reviewLikes);
+    }
+
+    public Optional<ReviewLikes> findReviewLikesById(Long id) {
+        return reviewLikesRepository.findById(id);
+    }
+    public List<ReviewLikes> findAllReviewLikes() {
+        return reviewLikesRepository.findAll();
+    }
+
+    public List<ReviewLikes> findReviewLikesByUserId(Long userId) {
+        return reviewLikesRepository.findReviewLikesByUserId(userId);
+    }
+
+    public boolean findByUserIdAndReviewId(Long userId, Long reviewId) {
+        return reviewLikesRepository.findByUserIdAndReviewId(userId, reviewId);
+    }
+
+    public boolean findByUserId(Long userId) {
+        return reviewLikesRepository.findByUserId(userId);
+    }
+
+    public void deleteReviewLikes(Long userId, Long reviewId) {
+        reviewLikesRepository.delete(userId, reviewId);
+    }
+
+    public void clearStore() {
+        reviewRepository.clearStroe();
+    }
+}
+
--- src/main/java/com/sunyoungeom/booktalk/service/ReviewService.java
+++ src/main/java/com/sunyoungeom/booktalk/service/ReviewService.java
@@ -1,59 +1,56 @@
 package com.sunyoungeom.booktalk.service;
 
 import com.sunyoungeom.booktalk.domain.Review;
-import com.sunyoungeom.booktalk.dto.ReviewLikesDTO;
-import com.sunyoungeom.booktalk.exception.GlobalExceptionHandler;
+import com.sunyoungeom.booktalk.domain.ReviewLikes;
 import com.sunyoungeom.booktalk.exception.ReviewException;
 import com.sunyoungeom.booktalk.exception.ReviewErrorCode;
+import com.sunyoungeom.booktalk.exception.UserErrorCode;
+import com.sunyoungeom.booktalk.exception.UserException;
 import com.sunyoungeom.booktalk.exception.common.CommonErrorCode;
-import com.sunyoungeom.booktalk.exception.common.CommonException;
-import com.sunyoungeom.booktalk.exception.common.ErrorCode;
-import com.sunyoungeom.booktalk.repository.ReviewRepository;
-import jakarta.servlet.http.HttpSession;
+import com.sunyoungeom.booktalk.facade.ReviewFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
-    private final ReviewRepository repository;
+    private final ReviewFacade reviewFacade;
 
     public Review createReview(Review review, Long userId, String username) {
         // 중복 리뷰 검증
         validateDuplicateReview(review.getTitle(), userId);
         // 리뷰 저장
-        repository.save(review);
+        reviewFacade.saveReview(review);
         return review;
     }
 
     private void validateDuplicateReview(String title, Long userId) {
-        if (repository.existsByTitleAndUserId(title, userId)) {
+        if (reviewFacade.existsReviewByTitleAndUserId(title, userId)) {
             throw new ReviewException(ReviewErrorCode.REVIEW_ALREADY_EXISTS_ERROR.getMessage());
         }
     }
 
     public List<Review> findAllOrderByDateDesc() {
-        return repository.findAllOrderByDateDesc();
+        return reviewFacade.findAllReviewsOrderByDateDesc();
     }
 
     public List<Review> findAllOrderByLikesDesc() {
-        return repository.findAllOrderByLikesDesc();
+        return reviewFacade.findAllReviewsOrderByLikesDesc();
     }
 
     public List<Review> findByTitleOrderByLikesDesc(String title) {
-        return repository.findByTitleOrderByLikesDesc(title);
+        return reviewFacade.findReviewsByTitleOrderByLikesDesc(title);
     }
 
     public List<Review> findByTitleOrderByDateDesc(String title) {
-        return repository.findByTitleOrderByDateDesc(title);
+        return reviewFacade.findReviewsByTitleOrderByDateDesc(title);
     }
 
     public List<Review> findByUserId(Long userId) {
-        return repository.findByUserId(userId);
+        return reviewFacade.findReviewsByUserId(userId);
     }
 
     public void update(Long reviewId, Long userId, String content) {
@@ -63,7 +60,7 @@ public class ReviewService {
         checkAuthorMatch(userId, review);
 
         review.setContent(content);
-        repository.update(review.getId(), content);
+        reviewFacade.updateReviewContent(review.getId(), content);
     }
 
     private void checkAuthorMatch(Long userId, Review review) {
@@ -78,12 +75,37 @@ public class ReviewService {
         // 작성자 일치 확인
         checkAuthorMatch(userId, review);
 
-        repository.deleteById(reviewId);
+        reviewFacade.deleteReviewById(reviewId);
     }
 
     private Review existsById(Long id) {
-        Review review = repository.findById(id)
+        Review review = reviewFacade.findReviewById(id)
                 .orElseThrow(() -> new ReviewException(ReviewErrorCode.REVIEW_NOT_FOUND_ERROR.getMessage()));
         return review;
+    }
+
+    public List<ReviewLikes> findAllReviewLikes() {
+        return reviewFacade.findAllReviewLikes();
+    }
+
+
+    public void likeReview(Long reviewId, Long userId) {
+        Review review = reviewFacade.findReviewById(reviewId)
+                .orElseThrow(() -> new ReviewException(ReviewErrorCode.REVIEW_NOT_FOUND_ERROR.getMessage()));
+        if (userId == null) {
+            throw new UserException(UserErrorCode.USER_NOT_FOUND_ERROR.getMessage());
+        }
+        if (review.getUserId().equals(userId)) {
+            throw new ReviewException("본인이 쓴 리뷰에는 좋아요를 누를 수 없습니다.");
+        }
+        boolean alreadyLiked = reviewFacade.findByUserIdAndReviewId(userId, reviewId);
+        if (alreadyLiked) {
+            review.setLikes(review.getLikes() - 1);
+            reviewFacade.deleteReviewLikes(userId, reviewId);
+        } else {
+            review.setLikes(review.getLikes() + 1);
+            ReviewLikes reviewLikes = new ReviewLikes(userId, reviewId);
+            reviewFacade.saveReviewLikes(reviewLikes);
+        }
     }
 }
