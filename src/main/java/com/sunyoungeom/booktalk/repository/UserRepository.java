e31e822b77608a13654b01d2c3c2dc68bc9dd861
sunyoung Eom <sunyoung.eom@outlook.com>
2024-07-07 06:54:04 +0900

    chore: 리포지토리 수정

--- src/main/java/com/sunyoungeom/booktalk/repository/UserRepository.java
+++ src/main/java/com/sunyoungeom/booktalk/repository/UserRepository.java
@@ -1,19 +1,57 @@
 package com.sunyoungeom.booktalk.repository;
 
 import com.sunyoungeom.booktalk.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public interface UserRepository {
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
 
-    void save(User user);
-    List<User> findAll();
-    Optional<User> findById(Long id);
-    void update(Long id, User updatedUser);
-    void delete(Long id);
-    boolean existsByNickname(String nickname);
-    boolean existsByEmail(String email);
-    Optional<User> findIdByEmail(String email);
+    private final SqlSessionTemplate sql;
 
+    public void save(User user) {
+         sql.insert("User.save", user);
+    }
+
+    public List<User> findAll() {
+        return sql.selectList("User.findAll");
+    }
+
+    public Optional<User> findById(Long id) {
+        User user = sql.selectOne("User.findById", id);
+        return Optional.ofNullable(user);
+    }
+
+    public void update(Long id, User updatedUser) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        params.put("updatedUser", updatedUser);
+        sql.update("User.update", params);
+    }
+
+    public void delete(Long id) {
+        sql.delete("User.delete", id);
+    }
+
+    public boolean existsByNickname(String nickname) {
+        Integer count = sql.selectOne("User.existsByNickname", nickname);
+        return count != null && count > 0;
+    }
+
+    public boolean existsByEmail(String email) {
+        Integer count = sql.selectOne("User.existsByEmail", email);
+        return count != null && count > 0;
+    }
+
+    public Optional<User> findIdByEmail(String email) {
+        User user = sql.selectOne("User.findIdByEmail", email);
+        return Optional.ofNullable(user);
+    }
 }
