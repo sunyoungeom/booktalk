b4a12fd0ec84ed4e801bd47dc6a5bb5a5ebc12d6
sunyoung Eom <sunyoung.eom@outlook.com>
2024-07-20 17:31:35 +0900

    build: no main manifest attribute 오류 수정을 위한 설정

---
+++ src/main/java/com/sunyoungeom/booktalk/BooktalkApplication.java
@@ -0,0 +1,13 @@
+package com.sunyoungeom.booktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktalkApplication.class, args);
+	}
+
+}
