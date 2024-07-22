101fd4073068e554d742e03608040edfe9b0680d
sunyoung Eom <sunyoung.eom@outlook.com>
2024-07-23 00:00:18 +0900

    feat: 스웨거를 위한 @OpenAPIDefinition 추가

--- src/main/java/com/sunyoungeom/booktalk/BooktalkApplication.java
+++ src/main/java/com/sunyoungeom/booktalk/BooktalkApplication.java
@@ -1,8 +1,11 @@
 package com.sunyoungeom.booktalk;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://https://3.36.235.110.nip.io", description = "북토크")})
 @SpringBootApplication
 public class BooktalkApplication {
 
