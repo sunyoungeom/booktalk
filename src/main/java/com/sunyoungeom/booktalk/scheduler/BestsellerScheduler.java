d8ed4cfe0bd86d0f0b54ece3e4f426d32cce17cc
sunyoung Eom <sunyoung.eom@outlook.com>
2024-07-22 22:43:14 +0900

    fix: 저장 경로 수정

--- src/main/java/com/sunyoungeom/booktalk/scheduler/BestsellerScheduler.java
+++ src/main/java/com/sunyoungeom/booktalk/scheduler/BestsellerScheduler.java
@@ -27,7 +27,7 @@ import java.util.*;
 @EnableScheduling
 @Slf4j
 public class BestsellerScheduler {
-    private static final String FILE_DIRECTORY = "/booktalk/file/best";
+    private static final String FILE_DIRECTORY = "/home/ubuntu/booktalk/file/img/";
     private static final String FILE_NAME = "bestseller_%s.json";
     private final Object lockObject = new Object();
 
