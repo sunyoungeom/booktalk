ad3db3a6fa5aabbed4988ec667981d0520b42a4b
sunyoung Eom <sunyoung.eom@outlook.com>
2024-07-23 00:27:18 +0900

    fix: 주소 수정

--- src/main/java/com/sunyoungeom/booktalk/BooktalkApplication.java
+++ src/main/java/com/sunyoungeom/booktalk/BooktalkApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://https://3.36.235.110.nip.io", description = "북토크")})
+@OpenAPIDefinition(servers = {@Server(url = "https://3.36.235.110.nip.io", description = "북토크")})
 @SpringBootApplication
 public class BooktalkApplication {
 
