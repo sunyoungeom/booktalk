431e1307ad9c996178630e1ed081abdf6ec3114a
sunyoung Eom <sunyoung.eom@outlook.com>
2024-07-23 01:33:34 +0900

    chore: 유저 에러처리 수정

--- src/main/java/com/sunyoungeom/booktalk/exception/ApiExceptionHandler.java
+++ src/main/java/com/sunyoungeom/booktalk/exception/ApiExceptionHandler.java
@@ -14,20 +14,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ReviewException.class)
-    public ResponseEntity<ErrorResponse> handleReviewException(ReviewException ex) {
-        ErrorCode errorCode = determineErrorCode(ex.getMessage(), "review");
-        ErrorResponse errorResponse = makeErrorResponse(errorCode);
-        log.warn("handleReviewException: code = {}, message = {}", errorCode.getCode(), ex.getMessage());
-        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
-    }
+//    @ExceptionHandler(ReviewException.class)
+//    public ResponseEntity<ErrorResponse> handleReviewException(ReviewException ex) {
+//        ErrorCode errorCode = determineErrorCode(ex.getMessage(), "review");
+////        ErrorResponse errorResponse = makeErrorResponse(errorCode);
+//        log.warn("handleReviewException: code = {}, message = {}", errorCode.getCode(), ex.getMessage());
+//        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
+//    }
 
     @ExceptionHandler(UserException.class)
     public ResponseEntity<ErrorResponse> handleUserException(UserException ex) {
-        ErrorCode errorCode = determineErrorCode(ex.getMessage(), "user");
-        ErrorResponse errorResponse = makeErrorResponse(errorCode);
-        log.warn("handleUserException: code = {}, message = {}", errorCode.getCode(), ex.getMessage());
-        return new ResponseEntity<>(errorResponse, errorCode.getHttpStatus());
+        ErrorResponse errorResponse = makeErrorResponse(ex);
+        log.warn("handleUserException: code = {}, message = {}", ex.getCode(), ex.getMessage());
+        return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
     }
 
     private ErrorCode determineErrorCode(String message, String ex) {
@@ -55,10 +54,10 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return CommonErrorCode.INTERNAL_SERVER_ERROR;
     }
 
-    private static ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+    private static ErrorResponse makeErrorResponse(UserException e) {
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(errorCode.getCode())
-                .message(errorCode.getMessage())
+                .code(e.getCode())
+                .message(e.getMessage())
                 .build();
         return errorResponse;
     }
--- src/main/java/com/sunyoungeom/booktalk/exception/UserException.java
+++ src/main/java/com/sunyoungeom/booktalk/exception/UserException.java
@@ -1,7 +1,27 @@
 package com.sunyoungeom.booktalk.exception;
 
+import com.sunyoungeom.booktalk.exception.common.CommonErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class UserException extends RuntimeException {
-    public UserException(String message) {
-        super(message);
+    private final HttpStatus httpStatus;
+    private final int code;
+
+
+    public UserException(UserErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.httpStatus = errorCode.getHttpStatus();
+        this.code = errorCode.getCode();
+    }
+
+    private int determineErrorCode(String message) {
+        for (UserErrorCode errorCode : UserErrorCode.values()) {
+            if (errorCode.getMessage().equals(message)) {
+                return errorCode.getCode();
+            }
+        }
+        return CommonErrorCode.INTERNAL_SERVER_ERROR.getCode();
     }
 }
